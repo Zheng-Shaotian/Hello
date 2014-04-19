5f24378bbf65eb3ef536c15738e29fa834c0223d
zhenghongyi <xiaodaier@yeah.net>
2014-04-19 12:01:36 +0800

    @sdfdsfsd

---
+++ testHell/testHell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testHell
+//
+//  Created by Hongyi Zheng on 4/19/14.
+//  Copyright (c) 2014 Hongyi Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
